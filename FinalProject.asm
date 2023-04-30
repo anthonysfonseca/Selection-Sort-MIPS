f15c7a146a1b4d097c39355ed7e2423d16bf8208
Anthony S Fonseca <107650911+anthonysfonseca@users.noreply.github.com>
2023-04-29 21:03:49 -0700

    Update FinalProject.asm

--- FinalProject.asm
+++ FinalProject.asm
@@ -6,11 +6,14 @@
 # s0 - array
 # s1 - sorted array
 # s2 - number of elements
+# s3 - number of elements - 1
 # t0 - temp storage for read int
 # t1 - loop counter
-# t2 - temp storage for current element
-# t3 - element counter + 1
-# t4 - element that the current element is being compared to
+# t2 - current element
+# t3 - compared element
+# t4 - counter for sorted array
+# t5 - counter for how far the compared element is from the current element
+
 
 .macro printString (%string) # prints the specified string
 	li $v0, 4
@@ -28,17 +31,33 @@
 	li $v0, 1
 	move $a0, $num
 	syscall
-.end_macro 
+.end_macro
+
+.macro resetArray
+	la $s0, array
+	move $t1, $zero
+.end_macro
+
+.macro print (%string)
+	li $v0, 4
+	.data
+	step: .asciiz %string
+	.text
+	la $a0, step
+	syscall
+.end_macro
 
 .data
 #numOfElements: .space 4 # Don't need this code as of now but leaving it just in case it's helpful in the future. It stores the number of array elements as a label.
 numOfElementsPrompt: .asciiz "How many integers will you be entering? "
 array: .word 0:100
+sortedArray: .word 0
 intPrompt: .asciiz "Enter integer "
 intPromptCont: .asciiz ": "
 printArray1: .asciiz "You entered: "
 comma: .asciiz ", "
 error: .asciiz "Incorrect Input. Try again.\n"
+temp: .asciiz " Code Went wrong here\n"
 
 .text
 main:
@@ -48,8 +67,9 @@ main:
 #	sw $t0, numOfElements # Don't need this code as of now but leaving it just in case it's helpful in the future. It stores the number of array elements as a label. 
 	move $s2, $t0 # stores the number of elements in $s2
 	addi $t1, $zero, 1 # loop counter starting at 1 so it can print the current element (e.g. starting at "Enter integer 1: " instead of "Enter integer 0: "
-	la $s0, array # loads the address of the array at $s0 (Not sure if this is needed, haven't tested without)
-	addi $t3, $s2, 1 # number of elements plus 1 so the loop accounts for starting at 1 instead of 0
+	la $s0, array # loads the address of the array at $s0
+	addi $s2, $s2, 1 # number of elements plus 1 so the loop accounts for starting at 1 instead of 0
+	la $s1, sortedArray
 	
 getArray:
 	printString(intPrompt) # prompts the user to enter the current element of the array
@@ -59,14 +79,13 @@ getArray:
 	sw $t0, 0($s0) # stores the input in the array
 	addi $s0, $s0, 4 # increments the base address for the next element
 	addi $t1, $t1, 1 # increments the loop counter
-	beq $t1, $t3, resetArrayAddress1 # moves to the next step in the code
+	beq $t1, $s2, resetArrayAddress1 # moves to the next step in the code
 	j getArray # restarts the loop
 
 resetArrayAddress1: # resets the array address back to the first element
-	addi $s0, $s0, -4 # decrements the base address to the previous element
-	addi $t1, $t1, -1 # decrements the loop counter
-	beq $t1, 1, echo # moves to the next step
-	j resetArrayAddress1 # restarts the loop until the array address is back down to the first element
+	resetArray
+	sub $s2, $s2, 1 # sets s2 back to the number of elements
+	add $s3, $s2, $zero # number of elements - 1
 
 echo:
 	printString(printArray1) # prints "You entered: " 
@@ -76,60 +95,61 @@ printArray: # loop for printing all array elements
 	printInt($t2) # prints the current array element
 	addi $s0, $s0, 4 # increments the base address for the next element
 	addi $t1, $t1, 1 # increments the loop counter
-	beq $t1, $t3, resetArrayAddress2 # resets the address again
+	beq $t1, $s2, resetArrayAddress2 # resets the address again
 	printString(comma) # prints ", " but not on the last element
 	j printArray # restarts the loop
 
-resetArrayAddress2: # not really made use of right now, but possibly useful for future code when you need to start at the first element
-	addi $s0, $s0, -4
-	addi $t1, $t1, -1
-	beq $t1, 1, EXIT
+resetArrayAddress2:
+	resetArray # resets $s0 to the base address of the array
+	la $s1, sortedArray
+	add $s1, $s1, $t4
+
+load:
+	lw $t2, 0($s0)
+
+sort:
+	lw $t2, 0($s0) # loads the current element into $t2
+	add $s0, $s0, 4 # shifts the base address to the next element
+	add $t1, $t1, 1 # increments the loop counter to the current place in the array
+	add $t5, $t5, 4
+	bge $t1, $s3, endArray
+	lw $t3, 0($s0)
+	blt $t3, $t2, switch
+	j sort
+
+switch:
+	sw $t3, 0($s1)
+	move $t5, $zero
+	j sort
+	
+endArray:
+	lw $t2, 0($s0)
+	sub $s0, $s0, $t5
+	sw $t2, 0($s0)
+	add $t4, $t4, 4
+	sub $s3, $s3, 1
+	bltz $s3, printSort
 	j resetArrayAddress2
-
-#sortArray:
-#	lw $t2, 0($s0)
-#	addi $s0, $s0, 4
-#	lw $t4, 0($s0)
-#	blt $t2, $t4, switchNum1
-#	bge $t2, $t4, sortArray
-
-#sortArrayCont:
-#	addi $t1, $t1, 1
-#	beq $t1, $t3, resetArrayAddress4sortArray
-	
-#switchNum1:
-#	sw $t2, 0($s0)
-#	move $t2, $t4
-#	j resetArrayAddress3
-
-#switchNum2:
-	
 
 invalid: # error when the user enters 0 or a negative number
 	printString(error)
 	j main
 
-#resetArrayAddress3:
-#	addi $s0, $s0, -4
-#	addi $t1, $t1, -1
-#	beq $t1, 1, switchNum2
-#	j resetArrayAddress3
+printSort:
+	la $s1, sortedArray
+	move $t0, $zero
+	print("\n")
+	print("Sorted array is: ") # prints specified string
 
-#resetArrayAddress4:
-#	addi $s0, $s0, -4
-#	addi $t1, $t1, -1
-#	beq $t1, 1, printSortedArray
-#	j resetArrayAddress4
+printSortedArray: # loop for printing all array elements
+	lw $t2, 0($s1) # loads the current array element
+	printInt($t2) # prints the current array element
+	addi $s1, $s1, 4 # increments the base address for the next element
+	addi $t1, $t1, 1 # increments the loop counter
+	beq $t1, $s2, exit # resets the address again
+	printString(comma) # prints ", " but not on the last element
+	j printSortedArray # restarts the loop
 
-#printSortedArray:
-#	lw $t2, 0($s0)
-#	printInt($t2)
-#	addi $s0, $s0, 4
-#	addi $t1, $t1, 1
-#	beq $t1, $t3, EXIT
-#	printString(comma)
-#	j printSortedArray
-
-EXIT:
+exit:
 	li $v0, 10
 	syscall
